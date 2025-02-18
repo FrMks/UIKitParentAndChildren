cd75761068488756cd491933c35449481a85747a
m.francuzov <maxfrancuzov@gmail.com>
2025-02-18 11:36:03 +0300

    Initial commit

---
+++ UIKitParentAndChildren/Module/FamalyScreen/View/FamalyScreen.swift
@@ -0,0 +1,16 @@
+//
+//  FamalyScreen.swift
+//  UIKitParentAndChildren
+//
+//  Created by Максим Французов on 18.02.2025.
+//
+
+import UIKit
+
+class FamalyScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
